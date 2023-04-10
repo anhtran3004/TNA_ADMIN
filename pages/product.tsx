ce540a2c8e3cdfd218e93df1d086f8fd0a60906d
Anh Tran <trananh30044003@gmail.com>
2023-04-10 18:08:41 +0700

    change UI product page

--- pages/product.tsx
+++ pages/product.tsx
@@ -1,63 +1,42 @@
-// import { Table, Thead, Tbody, Tr, Th, Td, Button } from '@chakra-ui/react';
+
 import Link from "next/link";
+import Layout from "@/components/Layout";
+import {useState} from "react";
+import {Product} from "@/components/HomeType";
 export default function Product(){
-    const products = [];
+    const [products, setProducts] = useState<Product[]>([])
     return (
-        // <Table>
-        //     <Thead>
-        //         <Tr>
-        //             <Th>ID</Th>
-        //             <Th>Name</Th>
-        //             <Th>Description</Th>
-        //             <Th>Price</Th>
-        //             <Th>Action</Th>
-        //         </Tr>
-        //     </Thead>
-        //     <Tbody>
-        //         {products.map((product) => (
-        //             <Tr key={product.id}>
-        //                 <Td>{product.id}</Td>
-        //                 <Td>{product.name}</Td>
-        //                 <Td>{product.description}</Td>
-        //                 <Td>{product.price}</Td>
-        //                 <Td>
-        //                     <Link href={`/admin/products/${product.id}`}>
-        //                         <Button size="sm" colorScheme="blue">
-        //                             Edit
-        //                         </Button>
-        //                     </Link>
-        //                 </Td>
-        //             </Tr>
-        //         ))}
-        //     </Tbody>
-        // </Table>
-        <table>
+        // eslint-disable-next-line react/jsx-no-undef
+    <Layout>
+        <table border={1}>
             <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Description</th>
-                    <th>Price</th>
-                    <th>Action</th>
-                </tr>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Action</th>
+            </tr>
             </thead>
             <tbody>
-                {products.map((product) => (
-                    <tr key={product.id}>
-                        <td>{product.id}</td>
-                        <td>{product.name}</td>
-                        <td>{product.description}</td>
-                        <td>{product.price}</td>
-                        <td>
-                            <Link href={`/admin/products/${product.id}`}>
-                                <Button size="sm" colorScheme="blue">
-                                    Edit
-                                </Button>
-                            </Link>
-                        </td>
-                    </tr>
-                ))}
+            {products.map((product) => (
+                <tr key={product.id}>
+                    <td>{product.id}</td>
+                    <td>{product.name}</td>
+                    <td>{product.description}</td>
+                    <td>{product.price}</td>
+                    <td>
+                        <Link href={`/admin/products/${product.id}`}>
+                            <button>
+                                Edit
+                            </button>
+                        </Link>
+                    </td>
+                </tr>
+            ))}
             </tbody>
         </table>
+    </Layout>
+
     );
 }
